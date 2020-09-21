706b1b348705e6ab6ffcf23dc65801cb819a6460
kevacoin <info@kevacoin.org>
2020-09-20 17:34:44 -0700

    Production build.

---
+++ docs/precache-manifest.3f44532a08a498afed286ecf062d2e5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd0bdd138205d71ade468f36b06a1ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05cb1fdb1ac9bb5f4951",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ce921f7d8d6267507569",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "05cb1fdb1ac9bb5f4951",
+    "url": "/static/js/2.205a64a2.chunk.js"
+  },
+  {
+    "revision": "748dd145ac2ea9378f0d2ff01847fceb",
+    "url": "/static/js/2.205a64a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce921f7d8d6267507569",
+    "url": "/static/js/main.bae6e0c2.chunk.js"
+  },
+  {
+    "revision": "e6e60c11ccbb93ecc810",
+    "url": "/static/js/runtime-main.0d06d938.js"
+  }
+]);
