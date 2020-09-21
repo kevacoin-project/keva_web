10e48f087e53b645bcd2c18c7559a6749669ddef
kevacoin <info@kevacoin.org>
2020-09-20 17:53:00 -0700

    Fixed relative path.

---
+++ docs/precache-manifest.fee22827ce4ddc4678b86265917a7af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3f49e54eb36b4bd8f20fa1f20cac6f",
+    "url": "/keva_web/index.html"
+  },
+  {
+    "revision": "5dd591053ce340388603",
+    "url": "/keva_web/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b4871e7686420b47e0f6",
+    "url": "/keva_web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5dd591053ce340388603",
+    "url": "/keva_web/static/js/2.993d4575.chunk.js"
+  },
+  {
+    "revision": "748dd145ac2ea9378f0d2ff01847fceb",
+    "url": "/keva_web/static/js/2.993d4575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4871e7686420b47e0f6",
+    "url": "/keva_web/static/js/main.f29d9704.chunk.js"
+  },
+  {
+    "revision": "cfd8c47375dcebe9874f",
+    "url": "/keva_web/static/js/runtime-main.291c2230.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.189f2b460c123fe48bef07379a567519.js"
+  "/keva_web/precache-manifest.fee22827ce4ddc4678b86265917a7af9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keva_web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
